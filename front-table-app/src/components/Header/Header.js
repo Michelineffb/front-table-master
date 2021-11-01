4ecc4c7772339eb0f7645e2b9f35577854a0f208
micheline-barros <michelinemms@gmail.com>
2021-11-01 15:00:57 -0300

    header

---
+++ front-table-app/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from "../../assets/img/bemobile.png";
+
+const Header = () => {
+    return(
+        <>
+            <img src={Logo} alt="Logo Be Mobile"/>
+        </>
+    )
+}
+export  default Header;
---
+++ front-table-app/src/pages/Funcionarios/Funcionarios.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+const Funcionarios = () => {
+    return(
+        <>
+            <Header />
+   
+        </>
+    )
+}
+export default Funcionarios;
