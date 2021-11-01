0a34097f6b63a00211b9b39aa529d3a6fe30c7c8
micheline-barros <michelinemms@gmail.com>
2021-11-01 15:02:53 -0300

    header estilização básica

--- front-table-app/src/components/Header/Header.js
+++ front-table-app/src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from "../../assets/img/bemobile.png";
+import { HeaderTag } from "./styled";
+
 
 const Header = () => {
     return(
-        <>
+        <HeaderTag>
             <img src={Logo} alt="Logo Be Mobile"/>
-        </>
+        </HeaderTag>
     )
 }
 export  default Header;
