2ad98dcfbe4d44d8b50dd411b4b562cf2075f964
micheline-barros <michelinemms@gmail.com>
2021-11-01 18:32:10 -0300

    page funcionários

---
+++ front-table-app/src/pages/Funcionarios/Funcionarios.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+const Funcionarios = () => {
+    return(
+        <>
+            <Header />
+            <main>
+
+            </main>
+        </>
+    )
+}
+export default Funcionarios;
